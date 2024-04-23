552232a31be52c3dc12810e1fcfc5b39a8c5ae5b
jiangxin <2649340476@qq.com>
2024-04-23 16:54:13 +0800

    1、Create CMC.  2、Create SaveMove Class.  3、Compressed flags.

---
+++ Source/TD_Zippy/Private/TD_CharacterMovementComponent.cpp
@@ -0,0 +1,91 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TD_CharacterMovementComponent.h"
+
+#include "GameFramework/Character.h"
+
+
+bool UTD_CharacterMovementComponent::FSavedMove_TD_Character::CanCombineWith(const FSavedMovePtr& NewMove,
+                                                                             ACharacter* InCharacter, float MaxDelta) const
+{
+	const FSavedMove_TD_Character* Temp_SaveMove = static_cast<FSavedMove_TD_Character*>(NewMove.Get());
+
+	if (Saved_bWantsToSprint != Temp_SaveMove->Saved_bWantsToSprint)
+	{
+		return false;
+	}
+	
+	return Super::CanCombineWith(NewMove, InCharacter, MaxDelta);
+}
+
+void UTD_CharacterMovementComponent::FSavedMove_TD_Character::Clear()
+{
+	Super::Clear();
+
+	Saved_bWantsToSprint = 0;
+}
+
+uint8 UTD_CharacterMovementComponent::FSavedMove_TD_Character::GetCompressedFlags() const
+{
+	uint8 Result = Super::GetCompressedFlags();
+
+	if (Saved_bWantsToSprint)
+	{
+		Result |= FLAG_Custom_0;
+	}
+	
+	return Result;
+}
+
+void UTD_CharacterMovementComponent::FSavedMove_TD_Character::SetMoveFor(ACharacter* C, float InDeltaTime,
+	FVector const& NewAccel, FNetworkPredictionData_Client_Character& ClientData)
+{
+	Super::SetMoveFor(C, InDeltaTime, NewAccel, ClientData);
+
+	if (UTD_CharacterMovementComponent* TempCMC = Cast<UTD_CharacterMovementComponent>(C->GetCharacterMovement()))
+	{
+		Saved_bWantsToSprint = TempCMC->Safe_bWantsToSprint;
+	}
+}
+
+void UTD_CharacterMovementComponent::FSavedMove_TD_Character::PrepMoveFor(ACharacter* C)
+{
+	Super::PrepMoveFor(C);
+
+	if (UTD_CharacterMovementComponent* TempCMC = Cast<UTD_CharacterMovementComponent>(C->GetCharacterMovement()))
+	{
+		TempCMC->Safe_bWantsToSprint = Saved_bWantsToSprint;
+	}
+}
+
+// Sets default values for this component's properties
+UTD_CharacterMovementComponent::UTD_CharacterMovementComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UTD_CharacterMovementComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UTD_CharacterMovementComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                                   FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
---
+++ Source/TD_Zippy/Public/TD_CharacterMovementComponent.h
@@ -0,0 +1,42 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "TD_CharacterMovementComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class TD_ZIPPY_API UTD_CharacterMovementComponent : public UCharacterMovementComponent
+{
+	GENERATED_BODY()
+
+	class FSavedMove_TD_Character : public FSavedMove_Character
+	{
+		typedef FSavedMove_Character Super;
+	public:
+		uint8 Saved_bWantsToSprint:1;
+
+		FSavedMove_TD_Character();
+
+		virtual bool CanCombineWith(const FSavedMovePtr& NewMove, ACharacter* InCharacter, float MaxDelta) const override;
+		virtual void Clear() override;
+		virtual uint8 GetCompressedFlags() const override;
+		virtual void SetMoveFor(ACharacter* C, float InDeltaTime, FVector const& NewAccel, FNetworkPredictionData_Client_Character& ClientData) override;
+		virtual void PrepMoveFor(ACharacter* C) override;
+	};
+
+public:
+	// Sets default values for this component's properties
+	UTD_CharacterMovementComponent();
+
+protected:
+	// ~Begin UActorComponent Interface
+	virtual void BeginPlay() override;
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	// ~End UActorComponent Interface
+
+public:
+	bool Safe_bWantsToSprint;
+};
