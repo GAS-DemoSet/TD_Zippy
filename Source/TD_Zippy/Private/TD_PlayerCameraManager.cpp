a673f1c5ec73d9054b6f35a0f2ad67cc53964b49
WIN-ST5EU1T38E7\Administrator <2649340476@qq.com>
2025-03-09 20:12:14 +0800

    feat: 平滑过渡蹲伏时得相机跳动

---
+++ Source/TD_Zippy/Private/TD_PlayerCameraManager.cpp
@@ -0,0 +1,42 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TD_PlayerCameraManager.h"
+
+#include "TD_CharacterMovementComponent.h"
+#include "TD_ZippyCharacter.h"
+#include "Components/CapsuleComponent.h"
+
+ATD_PlayerCameraManager::ATD_PlayerCameraManager(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
+
+void ATD_PlayerCameraManager::UpdateViewTarget(FTViewTarget& OutVT, float DeltaTime)
+{
+	Super::UpdateViewTarget(OutVT, DeltaTime);
+
+	// 当玩家蹲下时，平滑偏移相机镜头
+	if (ATD_ZippyCharacter* ZippyCharacter = Cast<ATD_ZippyCharacter>(GetOwningPlayerController()->GetPawn()))
+	{
+		UTD_CharacterMovementComponent* ZMC = ZippyCharacter->GetTD_CharacterMovement();
+		FVector TargetCrouchOffset = FVector(
+			0,
+			0,
+			ZMC->GetCrouchedHalfHeight() - ZippyCharacter->GetClass()->GetDefaultObject<ACharacter>()->GetCapsuleComponent()->GetScaledCapsuleHalfHeight()
+		);
+		FVector Offset = FMath::Lerp(FVector::ZeroVector, TargetCrouchOffset, FMath::Clamp(CrouchBlendTime / CrouchBlendDuration, 0.f, 1.f));
+
+		if (ZMC->IsCrouching())
+		{
+			CrouchBlendTime = FMath::Clamp(CrouchBlendTime + DeltaTime, 0.f, CrouchBlendDuration);
+			Offset -= TargetCrouchOffset;
+		}
+		else
+		{
+			CrouchBlendTime = FMath::Clamp(CrouchBlendTime - DeltaTime, 0.f, CrouchBlendDuration);
+		}
+
+		OutVT.POV.Location += Offset;
+	}
+}
---
+++ Source/TD_Zippy/Public/TD_PlayerCameraManager.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/PlayerCameraManager.h"
+#include "TD_PlayerCameraManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TD_ZIPPY_API ATD_PlayerCameraManager : public APlayerCameraManager
+{
+	GENERATED_BODY()
+
+public:
+	ATD_PlayerCameraManager(const FObjectInitializer& ObjectInitializer);
+	/** 计算给定视图目标的更新 POV */
+	virtual void UpdateViewTarget(FTViewTarget& OutVT, float DeltaTime) override;
+
+private:
+	/** 蹲伏过程中相机混合总持续时间 */
+	UPROPERTY(EditDefaultsOnly)
+	float CrouchBlendDuration = .2f;
+
+	/** 当前蹲伏相机已持续时间 */
+	float CrouchBlendTime = 0.f;
+};
